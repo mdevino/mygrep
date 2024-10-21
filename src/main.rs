9e7d30f9ea891b270cb97da40c9d5b60133fc89e
Mateus Devino <mateus@mdevino.com>
2024-10-20 21:09:59 -0300

    Move program logic to lib.rs
    Signed-off-by: Mateus Devino <mateus@mdevino.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::{env, error::Error, fs, process};
+use std::{env, process};
+
+use mygrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,33 +13,8 @@ fn main() {
     println!("Searching for{}", config.query);
     println!("In file {}", config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = mygrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
 }
