a8b734f16c30716f718782101863af2ff5aebc00
Mateus Devino <mateus@mdevino.com>
2024-10-21 12:03:33 -0300

    Remove prints and add failing search test
    Signed-off-by: Mateus Devino <mateus@mdevino.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
-use std::fs;
-
 use std::error::Error;
+use std::fs;
 
 pub struct Config {
     pub query: String,
@@ -22,7 +21,24 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
-
     Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
